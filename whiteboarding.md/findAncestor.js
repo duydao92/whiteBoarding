e8df798e87a26cfeb84a78c1dc87e8c549546ccc
duy <ddao0892@gmail.com>
2021-02-05 11:08:10 -0700

    first

---
+++ whiteboarding.md/findAncestor.js
@@ -0,0 +1,23 @@
+// ## Problem 5: Find Ancestors of a Node
+
+// Given a binary tree, find the ancestors of the node of a particular value.
+
+// Root will be a root node of a binary search tree and `k` is an integer value of
+// a node whose ancestors you will need to find.
+
+// Example:
+// findAncestors(6, 10) => [6, 9, 12]
+
+// ```js
+//   //   6
+//   //  / \
+//   // 4   9
+//   //   /  \
+//   //  8   12
+//   //      / \
+//   //     10  14
+// ```
+//input (root.val, target)
+//output (arr of parent nodes)
+
+//traverse with DFS
